4179a12b8574a948723e3ee4cbdb69d9ec77dd06
D <liudg@uci.edu>
2017-11-29 21:09:27 -0800

    Finished Part 3, also fixed an edgecase when history is empty

--- motionLoft.py
+++ motionLoft.py
@@ -1,14 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
-from random import *
 
 
-##Part 2 change the function into a class and add more methods
+import random
+import time
+
+##Part 3 Add the write to disk method
 class randOneFive():
     
     def __init__(self):
@@ -16,7 +11,7 @@ class randOneFive():
         
     #Generates a random number between 1 and 5 and inserts it into the history array  
     def generateRand(self):
-        randSeed = randint(0, 99);
+        randSeed = random.randint(0, 99);
         freqRate = [50,25,15,5,5]
         actualFreq = []
         for i in range(len(freqRate)):
@@ -31,14 +26,26 @@ class randOneFive():
             self.history.pop(0)
         self.history.append(num)
     
-    #return an Array the shows the frequency each number appears in the history
+    #return an Array the shows the frequency each number appears in the history.
+    #Return all 0's if there is no history
     def returnFrequency(self):
         frequency = []
         for i in range(1,6):
-            frequencyString = str(i) + ' is ' + str(self.history.count(i)/len(self.history)) + '%'
+            frequencyString = ''
+            if len(self.history) == 0:
+                frequencyString = str(i) + ' is 0%'
+            else:
+                frequencyString = str(i) + ' is ' + str(self.history.count(i)/len(self.history) * 100) + '%'
             frequency.append(frequencyString)
         return frequency
         
+    #writes the most recent random int in history plus the timestamp when this method was called
+    #if no history exists, do nothing. If file exists, append to the next line
+    def writeToDisk(self):
+        file = open('disk.txt','a')
+        if len(self.history) > 0:
+            #python index -1 points to the last/recent object in the array
+            recentRand = str(self.history[-1]) + '\t' + time.strftime("%a, %d %b %Y %H:%M:%S") + '\n'
+            file.write(recentRand)
+        file.close()   
 
-
-    
